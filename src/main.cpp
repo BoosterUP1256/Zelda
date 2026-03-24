6c2a420f9dd120c05a9ebf5bf537e35610087006
Kyle Rosenau <rosenau.kyle@gmail.com>
2026-03-24 19:29:59 -0400

    hello triangle

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 #include <iostream>
 
+#include <glad/gl.h>
+
 int main()
 {
     Gas::Platform::init();
@@ -42,6 +44,70 @@ int main()
             std::cout << "Middle\n";
         if (Gas::MouseListener::isMouseButtonReleased(Gas::MouseButton::Right))
             std::cout << "Right\n";
+
+        
+        // Render triangle!
+
+        float points[] = {
+             0.0f,  0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f
+        };
+
+        // vertex buffer object
+        GLuint vbo = 0;
+        glGenBuffers(1, &vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+        // vertex array object
+        GLuint vao = 0;
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+        // shaders
+        const char* vertex_shader =
+            "#version 410 core\n"
+            "in vec3 vp;"
+            "void main() {"
+            "  gl_Position = vec4( vp, 1.0 );"
+            "}";
+
+        const char* fragment_shader =
+            "#version 410 core\n"
+            "out vec4 frag_colour;"
+            "void main() {"
+            "  frag_colour = vec4( 0.5, 0.0, 0.5, 1.0 );"
+            "}";
+        
+        // compile vertex shader
+        GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vs, 1, &vertex_shader, NULL);
+        glCompileShader(vs);
+
+        // compile fragment shader
+        GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fs, 1, &fragment_shader, NULL);
+        glCompileShader(fs);
+
+        // link shader program
+        GLuint shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, fs);
+        glAttachShader(shaderProgram, vs);
+        glLinkProgram(shaderProgram);
+
+        // Draw tringle
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
         window.swapBuffers();
     }
