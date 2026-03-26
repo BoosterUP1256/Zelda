500ccd74ff953c6c380c86e9c9a11e1d92799ad5
Kyle Rosenau <rosenau.kyle@gmail.com>
2026-03-26 11:40:09 -0400

    Cleaning up Shader class

--- src/engine/core/renderer/Shader.h
+++ src/engine/core/renderer/Shader.h
@@ -13,7 +13,7 @@ struct ShaderProgramSource
 class Shader
 {
 public:
-    Shader(const std::string& filePath);
+    explicit Shader(std::string_view);
     ~Shader();
 
     void bind() const;
@@ -21,14 +21,14 @@ public:
 
     // uniforms
     // this system will probably change for actual game implementation
-    void setUniform4f(const std::string& name, float f0, float f1, float f2, float f3);
+    void setUniform4f(std::string_view name, float f0, float f1, float f2, float f3);
 
 
 private:
-    ShaderProgramSource _parseShader(const std::string& filePath);
-    uint32_t _compileShader(uint32_t type, const std::string& source);
-    uint32_t _createShader(const std::string& vertexShader, const std::string& fragmentShader);
-    int _getUniformLocation(const std::string& name);
+    ShaderProgramSource _parseShader(std::string_view filePath);
+    uint32_t _compileShader(uint32_t type, std::string_view source);
+    uint32_t _createShader(std::string_view vertexShader, std::string_view fragmentShader);
+    int _getUniformLocation(std::string_view name);
 
     uint32_t _rendererId;
     // caching system for uniforms
--- src/engine/platform/glfw/renderer/ShaderGL.cpp
+++ src/engine/platform/glfw/renderer/ShaderGL.cpp
@@ -1,16 +1,15 @@
 #include "core/renderer/Shader.h"
 
-#include <cstdint>
 #include <iostream>
 #include <sstream>
 #include <fstream>
 #include <glad/gl.h>
 
-Shader::Shader(const std::string& filePath)
-    : _filePath(filePath), _rendererId(0)
+Shader::Shader(const std::string_view filePath)
+    : _rendererId(0), _filePath(filePath)
 {
-    ShaderProgramSource source = _parseShader(filePath);
-    _rendererId = _createShader(source.vertexSource, source.fragmentSource);
+    auto [vertexSource, fragmentSource] = _parseShader(filePath);
+    _rendererId = _createShader(vertexSource, fragmentSource);
 }
 
 Shader::~Shader()
@@ -23,19 +22,18 @@ void Shader::bind() const
     glUseProgram(_rendererId);
 }
 
-void Shader::unbind() const
-{
+void Shader::unbind() const {
     glUseProgram(0);
 }
 
-void Shader::setUniform4f(const std::string& name, float f0, float f1, float f2, float f3)
+void Shader::setUniform4f(std::string_view name, float f0, float f1, float f2, float f3)
 {
     glUniform4f(_getUniformLocation(name), f0, f1, f2, f3);
 }
 
-ShaderProgramSource Shader::_parseShader(const std::string& filePath)
+ShaderProgramSource Shader::_parseShader(const std::string_view filePath)
 {
-    std::ifstream stream(filePath);
+    std::ifstream stream((filePath.data()));
 
     enum class ShaderType
     {
@@ -63,10 +61,10 @@ ShaderProgramSource Shader::_parseShader(const std::string& filePath)
     return { ss[0].str(), ss[1].str() };
 }
 
-uint32_t Shader::_compileShader(uint32_t type, const std::string& source)
+uint32_t Shader::_compileShader(uint32_t type, std::string_view source)
 {
     uint32_t id = glCreateShader(type);
-    const char* src = source.c_str();
+    const char* src = source.data();
     glShaderSource(id, 1, &src, NULL);
     glCompileShader(id);
 
@@ -79,7 +77,7 @@ uint32_t Shader::_compileShader(uint32_t type, const std::string& source)
         //char message[length];
         char* message = (char*)alloca(length * sizeof(char));
         glGetShaderInfoLog(id, length, &length, message);
-        // turnary operator kind of hacky here
+        // ternary operator kind of hacky here
         std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
         std::cout << message << std::endl;
         glDeleteShader(id);
@@ -89,7 +87,7 @@ uint32_t Shader::_compileShader(uint32_t type, const std::string& source)
     return id;
 }
 
-uint32_t Shader::_createShader(const std::string& vertexShader, const std::string& fragmentShader)
+uint32_t Shader::_createShader(std::string_view vertexShader, std::string_view fragmentShader)
 {
     uint32_t program = glCreateProgram();
     uint32_t vs = _compileShader(GL_VERTEX_SHADER, vertexShader);
@@ -107,15 +105,15 @@ uint32_t Shader::_createShader(const std::string& vertexShader, const std::strin
     return program;
 }
 
-int Shader::_getUniformLocation(const std::string& name)
+int Shader::_getUniformLocation(std::string_view name)
 {
-    if (_uniformLocationCache.find(name) != _uniformLocationCache.end())
-        return _uniformLocationCache[name];
+    if (_uniformLocationCache.contains(name.data()))
+        return _uniformLocationCache[name.data()];
     
-    uint32_t location = glGetUniformLocation(_rendererId, name.c_str());
+    int location = glGetUniformLocation(_rendererId, name.data());
     if (location == -1)
         std::cout << "Warning: uniform " << name << " doesn't exist!" << std::endl;
 
-    _uniformLocationCache[name] = location;
+    _uniformLocationCache[name.data()] = location;
     return location;
 }
